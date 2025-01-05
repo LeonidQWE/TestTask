8c6ffa46c32b1a7ecd4a2646fee45b7bbb4b9bd3
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-01-05 21:25:06 +0300

    fix: Tournament component

--- src/components/Tournament/Tournament.jsx
+++ src/components/Tournament/Tournament.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Tournament.module.less';
 import TournamentStatus from '../TournamentStatus/TournamentStatus';
@@ -5,9 +6,16 @@ import TournamentInfo from '../TournamentInfo/TournamentInfo';
 
 function Tournament ({ player }) {
   const { status, ...info } = player;
+  const [selected, setSelected] = useState(false);
+
+  const handleClickSlectTournament = () => {
+    setSelected(!selected);
+  }
 
   return (
-    <div className={styles.tournament}>
+    <div
+      className={`${styles.tournament} ${selected ? styles.selected : ''}`}
+      onClick={handleClickSlectTournament}>
       <TournamentStatus status={status} />
       <TournamentInfo info={info}/>
     </div>
